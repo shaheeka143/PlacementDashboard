eebd67a71916b1fc2181485ba96915eeb9dc14e2
shaheeka143 <shaheeka14@gmail.com>
2026-02-24 13:49:50 +0530

    Allow SECRET_KEY to be set via environment variable

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = "super_secure_secret_key_change_this"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "super_secure_secret_key_change_this")
     DB_PATH = "users.db"
     UPLOAD_DIR = "uploads"
